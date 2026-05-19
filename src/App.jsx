a909f05833ef321ee0a7a44df3b7054dc0849ef0
omarbougarne <omarbougarne98@gmail.com>
2026-05-19 22:41:22 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,34 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 // import './App.css'
 
 function App() {
   const [text, setText] = useState("");
-  const maxCharacters = 10;
-const handleChange = (e) => {
-    setText(e.target.value)
-}
+  const maxCharacters = 100;
+    const handleChange = (event) => {
+        if(maxCharacters > text.length){
+        setText(event.target.value)
+        }
+    }
+    const count = maxCharacters - text.length;
+    useEffect(() => {
+        document.title = count;
+    },[text]);
+
   return (
     <>
-    <div>
-        <h2>let's count your characters</h2>
-        <textarea
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-        cols={50}
-        rows={10}
-        />
-    </div>
         <div>
-        <span>current input number: {text.length} -- Max Character Input: {maxCharacters}</span>
+            <h2>let's count your characters</h2>
+            <textarea
+            value={text}
+            onChange={handleChange}
+            cols={50}
+            rows={10}
+            />
         </div>
-    </>
+        <div>
+        <span style={{color: count < 25 ? 'red' : count < 50 ? 'yellow' : 'green'}}>current input number: {text.length} -- Max Character Input: {maxCharacters}</span>
+        </div>
+ </>
   )
 }
 
