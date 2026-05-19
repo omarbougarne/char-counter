2a69414284624d3764430f094d0c65602b435b47
omarbougarne <omarbougarne98@gmail.com>
2026-05-19 21:52:24 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,25 @@ import { useState } from 'react'
 // import './App.css'
 
 function App() {
-  const [text, setText] = useState("")
-
+  const [text, setText] = useState("");
+  const maxCharacters = 10;
+const handleChange = (e) => {
+    setText(e.target.value)
+}
   return (
     <>
     <div>
         <h2>let's count your characters</h2>
         <textarea
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        cols={50}
+        rows={10}
         />
     </div>
+        <div>
+        <span>current input number: {text.length} -- Max Character Input: {maxCharacters}</span>
+        </div>
     </>
   )
 }
